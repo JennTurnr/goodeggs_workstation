38de292c0aaa9a5cf24ff485f418d8739472b830
Bob Zoller <bob@zoller.us>
2012-11-01 14:16:59 -0700

    add heroku-toolbelt to node app dependencies

--- recipes/node_app_dependencies.rb
+++ recipes/node_app_dependencies.rb
@@ -1,6 +1,7 @@
 include_recipe "pivotal_workstation::homebrew"
 
-brew_install "libpng"       # needed by jasmine-headless-webkit
-brew_install "phantomjs"    # integration tests
-brew_install "imagemagick"  # photo transformations
+brew_install "libpng"           # needed by jasmine-headless-webkit
+brew_install "phantomjs"        # integration tests
+brew_install "imagemagick"      # photo transformations
+brew_install "heroku-toolbelt"  # Heroku command line tools
 
