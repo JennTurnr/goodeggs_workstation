371b0760019b175a8606fcc1d2d0382da9f8869b
Bob Zoller <bob@zoller.us>
2013-05-16 12:22:49 -0700

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Bob Zoller"
+maintainer_email  "bob@goodeggs.com"
+license           "MIT"
+description       "Good Eggs Workstation Recipes"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0"
+supports          "mac_os_x"
+depends           "pivotal_workstation"
